8b485c238106ba6c00699759cc2b4aad037c13aa
Nurhadi Maulana <nurhadimaulana23@gmail.com>
2014-10-07 06:45:49 +0700

    Remove turbolinks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820120101) do
+ActiveRecord::Schema.define(version: 20140830044824) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.string   "keywords"
+    t.string   "description"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
